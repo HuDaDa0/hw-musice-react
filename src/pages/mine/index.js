66688354a36ae3f4e07fa5612d1303242ab6e115
HAHA <1497154694@qq.com>
2021-01-20 16:23:32 +0800

    未登录我的音乐

--- src/pages/mine/index.js
+++ src/pages/mine/index.js
@@ -1,12 +1,16 @@
 import React, { memo } from 'react'
 
+import { MineWrapper } from './style.js'
+
 
 function Mine() {
 
   return (
-    <div>
-      Mine
-    </div>
+    <MineWrapper>
+      <div className="n-pglg">
+        <div className="login-btn"></div>
+      </div>
+    </MineWrapper>
   )
 }
 
---
+++ src/pages/mine/style.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+
+export const MineWrapper = styled.div`
+
+  width: 980px;
+  min-height: 700px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid #d3d3d3;
+  border-width: 0 1px;
+  overflow: hidden;
+
+  .n-pglg {
+    width: 807px;
+    height: 268px;
+    margin: 0 auto;
+    margin-top: 104px;
+    background-position: 0 104px;
+    background: url('https://s2.music.126.net/style/web2/img/mymusic.png?a892de10f1756aa8a3031140bea53bcd') no-repeat center 0;
+    overflow: hidden;
+  }
+  .login-btn {
+    width: 168px;
+    height: 47px;
+    margin: 202px 0 0 482px;
+    background: url('https://s2.music.126.net/style/web2/img/mymusic.png?a892de10f1756aa8a3031140bea53bcd')  no-repeat 0 9999px;
+    background-position: 0 9999px;
+    text-indent: -9999px;
+    &:hover {
+      background-position: 0 -278px;
+    }
+  }
+
+
+
+
+`
+
