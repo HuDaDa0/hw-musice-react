2d37d74585640fcda3d0b5e32cb00e26950fff50
HAHA <1497154694@qq.com>
2021-01-20 15:55:24 +0800

    未登录的朋友页面

--- src/pages/friend/index.js
+++ src/pages/friend/index.js
@@ -1,12 +1,20 @@
 import React, { memo } from 'react'
 
+import { FriendWrapper } from './style.js'
 
 function Friend() {
 
   return (
-    <div>
-      Friend
-    </div>
+    <FriendWrapper>
+      <div className="n-pglg2">
+        <p>
+          你可以关注明星和好友品味他们的私房歌单
+          <br />
+          通过他们的动态发现更多精彩音乐
+        </p>
+        <div className="login-btn"></div>
+      </div>
+    </FriendWrapper>
   )
 }
 
---
+++ src/pages/friend/style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+
+export const FriendWrapper = styled.div`
+  width: 980px;
+  min-height: 700px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid #d3d3d3;
+  border-width: 0 1px;
+  overflow: hidden;
+
+  .n-pglg2 {
+    width: 902px;
+    height: 414px;
+    margin: 0 auto;
+    margin-top: 70px;
+    background-position: 0 70px;
+    background: url('https://s2.music.126.net/style/web2/img/notlogin.jpg?a14ce4a784fdb2576bb99f75c293f49c') no-repeat center 0;
+    p {
+      padding: 178px 0 0 555px;
+      color: #666;
+      font-size: 14px;
+    }
+  }
+
+  .login-btn {
+    width: 157px;
+    height: 48px;
+    margin: 36px 0 0 557px;
+    background: url('https://s2.music.126.net/style/web2/img/notlogin.jpg?a14ce4a784fdb2576bb99f75c293f49c') no-repeat center 0;
+    background-position: 0 9999px;
+    &:hover {
+      background-position: 0 -430px;
+    }
+  }
+
+`
+
+
+
