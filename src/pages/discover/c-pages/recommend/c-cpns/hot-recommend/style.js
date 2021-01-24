6e3211a6736f21085563bce7addf3656dc3cdb62
HAHA <1497154694@qq.com>
2021-01-24 14:01:10 +0800

    加一个边框

--- src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/style.js
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/style.js
@@ -4,10 +4,10 @@ import styled from 'styled-components'
 export const HotRecommendWrapper = styled.div`
 
   background-color: #ffffff;
-
   .hot-rec-left {
     width: 730px;
     padding: 20px 20px 40px;
+    border-right: 1px solid #d3d3d3;
     .song-list {
       margin-top: 20px;
       margin-left: -42px;
