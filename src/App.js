cf20f2665c20c4bcbdcd052fc3add897b5f3f0da
HAHA <1497154694@qq.com>
2021-03-17 18:24:25 +0800

    懒加载

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Suspense } from 'react'
 import { renderRoutes } from 'react-router-config'
 import { withRouter } from 'react-router-dom'
 
 import AppHeader from './components/app-header'
 import AppFooter from './components/app-footer'
 import AppPlayBar from './pages/player/app-player-bar'
+import { Skeleton } from 'antd';
 
 import routes from './router'
 
@@ -13,7 +15,9 @@ function App() {
     <div className="App">
       <AppHeader />
 
-      { renderRoutes(routes) }
+      <Suspense fallback={ <Skeleton active paragraph={{ rows: 10 }} /> }>
+        { renderRoutes(routes) }
+      </Suspense>
 
       <AppFooter />
       <AppPlayBar />
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,29 @@
+import React from 'react'
 import { Redirect } from 'react-router-dom'
 
-import Discover from '../pages/discover'
-import Friend from '../pages/friend'
-import Mine from '../pages/mine'
-import Player from '../pages/player'
+// import Discover from '../pages/discover'
+// import Friend from '../pages/friend'
+// import Mine from '../pages/mine'
+// import Player from '../pages/player'
 
-import Album from '../pages/discover/c-pages/album'
-import Artist from '../pages/discover/c-pages/artist'
-import Djradio from '../pages/discover/c-pages/djradio'
-import Ranking from '../pages/discover/c-pages/ranking'
-import Recommend from '../pages/discover/c-pages/recommend'
-import Songs from '../pages/discover/c-pages/songs'
+// import Album from '../pages/discover/c-pages/album'
+// import Artist from '../pages/discover/c-pages/artist'
+// import Djradio from '../pages/discover/c-pages/djradio'
+// import Ranking from '../pages/discover/c-pages/ranking'
+// import Recommend from '../pages/discover/c-pages/recommend'
+// import Songs from '../pages/discover/c-pages/songs'
+
+const Discover = React.lazy(() => import('../pages/discover'))
+const Friend = React.lazy(() => import('../pages/friend'))
+const Mine = React.lazy(() => import('../pages/mine'))
+const Player = React.lazy(() => import('../pages/player'))
+
+const Album = React.lazy(() => import('../pages/discover/c-pages/album'))
+const Artist = React.lazy(() => import('../pages/discover/c-pages/artist'))
+const Djradio = React.lazy(() => import('../pages/discover/c-pages/djradio'))
+const Ranking = React.lazy(() => import('../pages/discover/c-pages/ranking'))
+const Recommend = React.lazy(() => import('../pages/discover/c-pages/recommend'))
+const Songs = React.lazy(() => import('../pages/discover/c-pages/songs'))
 
 
 const routes = [
